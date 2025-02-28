81c49f7827eaac5b95f5bed81d42c1ce65a94582
ZS iMac <zsolis@deltacontrols.com>
2025-02-27 23:05:54 -0800

    Fixed footer and index button.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { PageContainer } from "@/components/common/PageContainer";
 import { Button } from "@/components/ui/button";
 import { LandingHeader } from "@/features/landing/components/LandingHeader";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Link } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -17,10 +17,16 @@ function App() {
         keywords="app, landing, page, first, welcome"
       >
         <div className="w-full h-96 flex flex-col items-center justify-center gap-8">
-          <Button variant={"outline"} size={"lg"}>
-            Start a new Estimate
-          </Button>
-          <Button variant={"outline"} size={"lg"}>
+          <Link to="/new">
+            <Button variant={"outline"} size={"lg"}>
+              Start a new Estimate
+            </Button>
+          </Link>
+          <Button
+            variant={"outline"}
+            size={"lg"}
+            onClick={() => console.log("We can load from JSON here")}
+          >
             Load From JSON
           </Button>
         </div>
